b275512f5adc6f6f9712a392b813b6fa8e1dae95
krasulna-coder <krasulya.n@roistat.com>
2026-03-20 17:27:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,76 +1,36 @@
-class ActionButton {
-    constructor(element) {
-        this.element = element;
-        this.action = element.dataset.action;
+class Button {
+  constructor(el) {
+    this.el = el;
+    this.action = el.dataset.action;
+    this.init();
+  }
 
-        this.init();
-    }
+  init() {
+    this.el.addEventListener("click", () => this.handleClick());
+  }
 
-    init() {
-        this.element.addEventListener('click', () => this.handleClick());
-    }
+  handleClick() {
+    console.log("Clicked:", this.action);
+    this.execute();
+  }
 
-    handleClick() {
-        console.log(`Нажата кнопка: ${this.action}`);
-        this.execute();
-    }
-
-    execute() {
-        alert(`Функционал "${this.action}" пока не реализован`);
-    }
-}
-
-class ApiTestButton extends ActionButton {
-    execute() {
-        alert("Тестирование API — скоро будет логика");
-    }
-}
-
-class DocsButton extends ActionButton {
-    execute() {
-        alert("Документация — в разработке");
-    }
-}
-
-class ProductRequestButton extends ActionButton {
-    execute() {
-        alert("Запрос в продукт — заглушка");
-    }
-}
-
-class BugReportButton extends ActionButton {
-    execute() {
-        alert("Баг-репорт — заглушка");
-    }
-}
-
-class DevTaskButton extends ActionButton {
-    execute() {
-        alert("Таск в разработку — заглушка");
-    }
+  execute() {
+    alert(`"${this.action}" пока не реализовано`);
+  }
 }
 
 class ButtonFactory {
-    static create(element) {
-        const action = element.dataset.action;
-
-        switch (action) {
-            case 'api': return new ApiTestButton(element);
-            case 'docs': return new DocsButton(element);
-            case 'product': return new ProductRequestButton(element);
-            case 'bug': return new BugReportButton(element);
-            case 'task': return new DevTaskButton(element);
-            default: return new ActionButton(element);
-        }
-    }
+  static create(el) {
+    return new Button(el);
+  }
 }
 
 class App {
-    init() {
-        document.querySelectorAll('.btn').forEach(el => {
-            ButtonFactory.create(el);
-        });
-    }
+  init() {
+    document.querySelectorAll("button").forEach(btn => {
+      ButtonFactory.create(btn);
+    });
+  }
 }
 
 new App().init();
